6a4d096c59ad6ad4a8f464331849fab2ef1043d9
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2021-02-24 17:00:16 -0300

    pequeno teste na main (erro: arquivo pula direta pra ultima linha)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <vector>
+#include "CoordinatesData.h"
+#include "DataReader.h"
+#include "QuadTree.h"
 
 using namespace std;
 
 int main()
 {
+    vector<CoordinatesData*> data;
+    DataReader reader;
+    data = reader.readCoordinatesDatafromFile("brazil_cities_coordinates.csv");
+    QuadTree *tree = new QuadTree(*data[0]);
+    for(int i = 1;i < data.size();i++)
+    {
+        tree->insert(*data[i]);
+    }
+    
     return 0;
 }
