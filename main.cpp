2a4836290a1d331ba01d6f4947742e38d9139754
Marcelo <mgscosta@ice.ufjf.br>
2021-03-14 01:10:53 -0300

    adiciona calculo do runtime

--- main.cpp
+++ main.cpp
@@ -12,35 +12,50 @@
 
 using namespace std;
 
-void execAVLBenchmark (int nData) {
-  vector<int> randomIndex;
-  vector<int> cities;
-  Hash hashTable;
-  ArvoreAVL avl;
-  string cityCode;
-  long int * comparacoes;
-  long int numCasos = 0;
-  // for(int i < m (5); i++ {})
-  // Inserção
-  for(int i=0; i < nData; i++) {
-    // avl.insere(rand() % hashTable.getSize());
-    // Insere index da tabela Hash na AVL tree
-  }
-  //S1
-  cout << "Digite o código da cidade" << endl;
-  cin >> cityCode;
-  cities = avl.buscaPorTotaldeCasos(cityCode, comparacoes, hashTable);
-  for(int i =0; i < cities.size(); i++) {
-      numCasos += hashTable.getItemFromHashKey(cities[i]).getCaseCount();
-  }
+void execAVLBenchmark(int nData)
+{
+    vector<int> randomIndex;
+    vector<int> cities;
+    Benchmark bench;
+    Hash hashTable;
+    ArvoreAVL avl;
+    string cityCode;
+    long int *comparacoes;
+    long int numCasos = 0;
+    
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "M =  " << i << endl;
+        // Inserção
+        bench.setStartTimeAsNow();
+        for (int i = 0; i < nData; i++)
+        {
+            avl.insere(rand() % hashTable.getSize(), hashTable); // Insere index da tabela Hash na AVL tree   
+        }
+        bench.setEndTimeAsNow();
+        bench.generateInsertionRuntime();   // Calcula tempo de execução da inserção
+        cout << "Digite o código da cidade" << endl;
+        cin >> cityCode;    // Pega codigo da cidade via input do usuário
+    
+        bench.setStartTimeAsNow();
+        cities = avl.buscaPorTotaldeCasos(cityCode, comparacoes, hashTable); // Busca S1
+        bench.setEndTimeAsNow();
+        bench.generateSearchRuntime(); // Calcula tempo de execução da busca
+
+        for (int i = 0; i < cities.size(); i++)
+        {
+            numCasos += hashTable.getItemFromHashKey(cities[i]).getCaseCount();
+        }
+  //  }
 }
 
 int main(int argc, char const *argv[])
 {
-    if(argc != 3)
+    if (argc != 3)
     {
         cout << "ERRO: Esperando os seguintes parâmetros: ./<nome_do_executável> <diretório_dos_datasets> <modo_de_execução>" << endl;
-        cout << "0 - Modo de testes" << endl << "1 - Modo de Benchmark" << endl;
+        cout << "0 - Modo de testes" << endl
+             << "1 - Modo de Benchmark" << endl;
     }
     else
     {
@@ -57,17 +72,17 @@ int main(int argc, char const *argv[])
         cout << "Diretório: " << directory << endl;
 
         cout << "Importando arquivo de coordenadas do diretório..." << endl;
-        vector<CoordinatesData*> coordinatesData = reader.readCoordinatesDatafromFile(directory + "brazil_cities_coordinates.csv");
+        vector<CoordinatesData *> coordinatesData = reader.readCoordinatesDatafromFile(directory + "brazil_cities_coordinates.csv");
         cout << "Importando arquivo de dados da COVID-19 do diretório..." << endl;
-        vector<HashItem*> covidData = reader.readPreProcessedCovidDataFromFile(directory + "brazil_covid19_cities_processado.csv");
+        vector<HashItem *> covidData = reader.readPreProcessedCovidDataFromFile(directory + "brazil_covid19_cities_processado.csv");
         cout << "Criando tabela de referência para inserção e busca em árvore..." << endl;
         Hash tableForTrees;
-        for(int i = 0;i < covidData.size();i++)
+        for (int i = 0; i < covidData.size(); i++)
         {
             tableForTrees.insert(covidData[i]);
         }
 
-        if(atoi(argv[2]) == 0)
+        if (atoi(argv[2]) == 0)
         {
             bool run = true;
             while (run)
@@ -79,8 +94,11 @@ int main(int argc, char const *argv[])
                 ArvoreAVL avlTree;
                 BTree bTree(31);
                 vector<int> randomIndex;
-                cout << "Menu:" << endl << "1 - Inserção de N cidades da Quad Tree" << endl << "2 - Inserção de N registros na tabela hash" <<
-                endl << "3 - Inserção de N chaves na árvore AVL" << endl << "4 - Inserção de N chaves na árvore B" << endl;
+                cout << "Menu:" << endl
+                     << "1 - Inserção de N cidades da Quad Tree" << endl
+                     << "2 - Inserção de N registros na tabela hash" << endl
+                     << "3 - Inserção de N chaves na árvore AVL" << endl
+                     << "4 - Inserção de N chaves na árvore B" << endl;
                 cout << "Selecione uma opção: ";
                 cin >> option;
 
@@ -90,94 +108,94 @@ int main(int argc, char const *argv[])
                     cout << "Selecione o número de registros a ser inseridos na Quad Tree: ";
                     cin >> n;
                     srand(time(NULL));
-                    for(int i = 0; i < n; i++)
+                    for (int i = 0; i < n; i++)
                     {
                         randomIndex.push_back(rand() % coordinatesData.size());
                     }
-                    for(int i = 0; i < randomIndex.size();i++)
+                    for (int i = 0; i < randomIndex.size(); i++)
                     {
-                        quadTree.insert(*coordinatesData[randomIndex[i]]);   
+                        quadTree.insert(*coordinatesData[randomIndex[i]]);
                     }
 
-                    if(n <= 20)
+                    if (n <= 20)
                     {
                         quadTree.printTree(false);
                     }
                     else
                     {
-                        quadTree.printTree(true,directory + "saidasQuadTree.txt");
+                        quadTree.printTree(true, directory + "saidasQuadTree.txt");
                     }
                     break;
-                
+
                 case 2:
                     cout << "Selecione o número de registros a ser inseridos na Tabela Hash: ";
                     cin >> n;
                     srand(time(NULL));
-                    for(int i = 0; i < n; i++)
+                    for (int i = 0; i < n; i++)
                     {
                         randomIndex.push_back(rand() % covidData.size());
                     }
-                    for(int i = 0; i < randomIndex.size();i++)
+                    for (int i = 0; i < randomIndex.size(); i++)
                     {
-                        hashTable.insert(covidData[randomIndex[i]]);   
+                        hashTable.insert(covidData[randomIndex[i]]);
                     }
 
-                    if(n <= 20)
+                    if (n <= 20)
                     {
                         hashTable.print(false);
                     }
                     else
                     {
-                        hashTable.print(true,directory + "saidasHashTable.txt");   
+                        hashTable.print(true, directory + "saidasHashTable.txt");
                     }
                     break;
-                
+
                 case 3:
                     cout << "Selecione o número de chaves a ser inseridos na Árvore AVL: ";
                     cin >> n;
                     srand(time(NULL));
-                    for(int i = 0; i < n; i++)
+                    for (int i = 0; i < n; i++)
                     {
                         randomIndex.push_back(rand() % covidData.size());
                     }
-                    for(int i = 0; i < randomIndex.size();i++)
+                    for (int i = 0; i < randomIndex.size(); i++)
                     {
-                        avlTree.insere(randomIndex[i],tableForTrees);   
+                        avlTree.insere(randomIndex[i], tableForTrees);
                     }
 
-                    if(n <= 20)
+                    if (n <= 20)
                     {
                         avlTree.imprime(false);
                     }
                     else
                     {
-                        avlTree.imprime(true,directory + "saidasAVLTree.txt");
+                        avlTree.imprime(true, directory + "saidasAVLTree.txt");
                     }
                     break;
-                
+
                 case 4:
                     cout << "Selecione o número de chaves a ser inseridos na Árvore B: ";
                     cin >> n;
                     srand(time(NULL));
-                    for(int i = 0; i < n; i++)
+                    for (int i = 0; i < n; i++)
                     {
                         randomIndex.push_back(rand() % covidData.size());
                     }
-                    for(int i = 0; i < randomIndex.size();i++)
+                    for (int i = 0; i < randomIndex.size(); i++)
                     {
-                        bTree.insert(randomIndex[i],tableForTrees);   
+                        bTree.insert(randomIndex[i], tableForTrees);
                     }
 
-                    if(n <= 20)
+                    if (n <= 20)
                     {
                         bTree.printTree(false);
                     }
                     else
                     {
-                        bTree.printTree(true,directory + "saidasBTree.txt");   
+                        bTree.printTree(true, directory + "saidasBTree.txt");
                     }
-                    break;           
-                
+                    break;
+
                 default:
                     cout << "Encerrando execução..." << endl;
                     run = false;
@@ -185,7 +203,7 @@ int main(int argc, char const *argv[])
                 }
             }
         }
-        else if(atoi(argv[2]) == 1)
+        else if (atoi(argv[2]) == 1)
         {
             // execAVLBenchmark(10000)
             // execAVLBenchmark(50000)
@@ -204,12 +222,11 @@ int main(int argc, char const *argv[])
             // execB200Benchmark(100000)
             // execB200Benchmark(500000)
             // execB200Benchmark(1000000)
-
-
         }
         else
         {
-            cout << "Modo Inválido" << endl << "Encerrando execução..." << endl;
+            cout << "Modo Inválido" << endl
+                 << "Encerrando execução..." << endl;
         }
     }
     return 0;
